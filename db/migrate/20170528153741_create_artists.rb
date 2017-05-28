caf60d67c7b12dc840c125af713cbec192d63b21
thkocks <timhkocks@gmail.com>
2017-05-28 18:04:16 +0200

    Add pages

---
+++ db/migrate/20170528153741_create_artists.rb
@@ -0,0 +1,12 @@
+class CreateArtists < ActiveRecord::Migration[5.1]
+  def change
+    create_table :artists do |t|
+      t.string :name
+      t.string :genre
+      t.text :bio
+      t.string :image_url
+
+      t.timestamps
+    end
+  end
+end

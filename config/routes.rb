fa813126395035e4ad2737e24b7461eab4477268
thkocks <timhkocks@gmail.com>
2017-05-28 19:30:12 +0200

    Add songs

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
 
   resources :profiles, only: [:new, :edit, :create, :update]
   resources :artists
+  resources :songs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170528160547) do
+ActiveRecord::Schema.define(version: 20170528165045) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,18 @@ ActiveRecord::Schema.define(version: 20170528160547) do
     t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
+  create_table "songs", force: :cascade do |t|
+    t.string "title"
+    t.string "album"
+    t.string "genre"
+    t.decimal "song_length"
+    t.string "image_url"
+    t.bigint "artist_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["artist_id"], name: "index_songs_on_artist_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -52,4 +64,5 @@ ActiveRecord::Schema.define(version: 20170528160547) do
   end
 
   add_foreign_key "profiles", "users"
+  add_foreign_key "songs", "artists"
 end

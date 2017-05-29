a9491cda1f85f61b8a6ee892dcbb22a274aa502a
thkocks <timhkocks@gmail.com>
2017-05-29 05:28:54 +0200

    rehost images

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,25 +15,25 @@ carla = User.create(email: "carla@musify.com", password: "abcd1234")
 skrillex = Artist.create!(name: "Skrillex",
                 genre: "Dubstep",
                 bio: "Sonny John Moore (born January 15, 1988), known professionally as Skrillex, is an American electronic dance music producer, DJ, singer, songwriter and multi-instrumentalist.",
-                image_url: "https://upload.wikimedia.org/wikipedia/commons/b/ba/Skrillex.jpg"
+                image_url: "http://res.cloudinary.com/thkocks/image/upload/v1496028266/Skrillex_kocefz.jpg"
 )
 
 blur = Artist.create!(name: "Blur",
                 genre: "Rock",
                 bio: "Blur are an English rock band, formed in London in 1988. The group consists of singer/keyboardist Damon Albarn, guitarist/singer Graham Coxon, bassist Alex James and drummer Dave Rowntree. Their debut album Leisure (1991) incorporated the sounds of Madchester and shoegazing. Following a stylistic change influenced by English guitar pop groups such as the Kinks, the Beatles and XTC, Blur released Modern Life Is Rubbish (1993), Parklife (1994) and The Great Escape (1995). In the process, the band became central to the Britpop music and culture movement, and achieved mass popularity in the UK.",
-                image_url: "https://upload.wikimedia.org/wikipedia/commons/d/d5/Blur_%28band%29.png"
+                image_url: "http://res.cloudinary.com/thkocks/image/upload/v1496028254/Blur__band_tdha4x.png"
 )
 
 gangstarr = Artist.create!(name: "Gang Starr",
                 genre: "Hiphop",
                 bio: "Gang Starr was an influential East Coast hip hop duo that consisted of MC Guru and the DJ/producer DJ Premier.",
-                image_url: "https://upload.wikimedia.org/wikipedia/commons/4/42/Gangstarr-09.jpg"
+                image_url: "http://res.cloudinary.com/thkocks/image/upload/v1496028253/Gangstarr-09_ubhtdz.jpg"
 )
 
 atribecalledquest = Artist.create!(name: "A Tribe Called Quest",
                 genre: "Hiphop",
                 bio: "A Tribe Called Quest is an American hip-hop group formed in 1985[2] and originally composed of MC and producer Q-Tip, MC Phife Dawg and DJ and producer Ali Shaheed Muhammad. A fourth member, MC Jarobi White, left the group in 1991 after the release of their debut album. He continued to contribute to the band sporadically before rejoining for their 2006 reunion. Along with De La Soul, the group was a central part of the Native Tongues, and enjoyed the most commercial success out of all the groups to emerge from that collective. The group released six albums between 1990 and 2016. The band broke up in 1998 after releasing their fifth album. In 2006, the group reunited and toured the United States. In 2016, they released their sixth album, which was still incomplete when Phife Dawg died suddenly in March 2016, and was completed by the other members after his death.",
-                image_url: "https://media.npr.org/assets/img/2011/07/21/32_1_wide-25b7986ab71a904c24acc2cf368315016c8e3137.jpg?s=1400"
+                image_url: "http://res.cloudinary.com/thkocks/image/upload/v1496028256/32_1_wide-25b7986ab71a904c24acc2cf368315016c8e3137_ka3dte.jpg"
 )
 
 # Artist.create!(name: "",
